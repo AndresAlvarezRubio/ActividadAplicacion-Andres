dd999c6a55a186ab256db16db738a3bdfa05b80e
Andrés Álvarez <andresalvarezrubio@gmail.com>
2024-05-24 15:01:41 +0200

    Intento de hacer ecuacion de segundo grado

--- script/actividadDeAplicacion2_12.js
+++ script/actividadDeAplicacion2_12.js
@@ -1,1 +1,15 @@
 //Crea un programa que calcule operaciones de segundo grado
+
+let a= Number(prompt("Valor de a"));
+let b= Number(prompt("Valor de b"));
+let c= Number(prompt("Valor de c"));
+
+let formulaPositivo = ((b*-1)+Math.sqrt((b**2)-(4*a*c)))/2*a;
+let formulaNegativo = ((b*-1)-Math.sqrt((b**2)-(4*a*c)))/2*a;
+
+console.log(formulaNegativo);
+console.log(formulaPositivo);
+
+
+
+//Me rindo, tiene muchas cosas y ya no estoy para pensar jajaja
